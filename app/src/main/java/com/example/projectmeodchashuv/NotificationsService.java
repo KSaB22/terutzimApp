41121f558eff9be826a75e501a81ad2d1161f6bb
Kfir Back <3backids@gmail.com>
2020-12-28 23:02:45 +0200

    notification update

--- app/src/main/java/com/example/projectmeodchashuv/NotificationsService.java
+++ app/src/main/java/com/example/projectmeodchashuv/NotificationsService.java
@@ -10,9 +10,11 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.IBinder;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,12 +39,12 @@ NotificationsService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference dbRef = database.getReference("teruzim");
 
-        dbRef.addValueEventListener(new ValueEventListener() {
-            @Override
+        /*dbRef.addValueEventListener(new ValueEventListener() {
 
+            @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
@@ -50,6 +52,7 @@ NotificationsService extends Service {
                     first = false;
                 }
                 else{
+
                     int NOTIFICATION_ID = 234;
                     NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                     String CHANNEL_ID = "Terutz";
@@ -84,9 +87,106 @@ NotificationsService extends Service {
 
             }
 
+
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
+
+            }
+        });*/
+        dbRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if(first){
+                    first = false;
+                }
+                else{
+
+                    int NOTIFICATION_ID = 234;
+                    NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    String CHANNEL_ID = "Terutz";
+
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                        CharSequence name = "Terutz";
+                        String Description = "Terutzim channel";
+                        int importance = NotificationManager.IMPORTANCE_HIGH;
+                        NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+                        mChannel.setDescription(Description);
+                        mChannel.enableLights(true);
+                        mChannel.setLightColor(Color.RED);
+                        mChannel.enableVibration(true);
+                        mChannel.setShowBadge(true);
+                        notificationManager.createNotificationChannel(mChannel);
+                    }
+
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentTitle("Terutz")
+                            .setContentText("one Terutz has been added");
+
+                    Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
+                    stackBuilder.addParentStack(MainActivity.class);
+                    stackBuilder.addNextIntent(resultIntent);
+                    PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.setContentIntent(resultPendingIntent);
+                    notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if(first){
+                    first = false;
+                }
+                else{
+
+                    int NOTIFICATION_ID = 234;
+                    NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    String CHANNEL_ID = "Terutz";
+
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                        CharSequence name = "Terutz";
+                        String Description = "Terutzim channel";
+                        int importance = NotificationManager.IMPORTANCE_HIGH;
+                        NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+                        mChannel.setDescription(Description);
+                        mChannel.enableLights(true);
+                        mChannel.setLightColor(Color.RED);
+                        mChannel.enableVibration(true);
+                        mChannel.setShowBadge(true);
+                        notificationManager.createNotificationChannel(mChannel);
+                    }
+
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentTitle("Terutz")
+                            .setContentText("one Terutz has been changed");
+
+                    Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
+                    stackBuilder.addParentStack(MainActivity.class);
+                    stackBuilder.addNextIntent(resultIntent);
+                    PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.setContentIntent(resultPendingIntent);
+                    notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+                }
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
